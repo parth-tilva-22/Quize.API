ee966126f81b4c830e4baff39f1bd6e460148b75
Parth Tilva <Parth.Tilva@mrisoftware.com>
2026-09-09 23:16:49 +0530

    added questions controller

---
+++ Dtos/OptionsDto.cs
@@ -0,0 +1,9 @@
+﻿using Quiz.API.Models;
+
+namespace Quiz.API.Dtos {
+  public class OptionsDto {
+    public required string Text { get; set; }
+    public bool IsCorrect { get; set; }
+    public int? QuestionId { get; set; } = 0;
+  }
+}
---
+++ Migrations/20260903001618_made quizeId optional in quetions class.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Quiz.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class madequizeIdoptionalinquetionsclass : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Questions_Quizzes_QuizId",
+                table: "Questions");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "QuizId",
+                table: "Questions",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Questions_Quizzes_QuizId",
+                table: "Questions",
+                column: "QuizId",
+                principalTable: "Quizzes",
+                principalColumn: "QuizId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Questions_Quizzes_QuizId",
+                table: "Questions");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "QuizId",
+                table: "Questions",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Questions_Quizzes_QuizId",
+                table: "Questions",
+                column: "QuizId",
+                principalTable: "Quizzes",
+                principalColumn: "QuizId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
